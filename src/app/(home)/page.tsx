5ba1ed3f48e3519f6dac182fc031aa3812a7c477
Wolace Luedke <wolacepin@hotmail.com>
2023-10-22 14:32:20 -0300

    feat: add banner to home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { useSession } from 'next-auth/react'
+import Image from 'next/image'
+
+export default function Home() {
+return <div className='p-5'>
+    <Image 
+    src="/banner-home-01.png"
+    height={0}
+    width={0}
+    className='h-auto w-full'
+    sizes='100vw'
+    alt='até 55% desconto só esse mês'
+    />
+</div>
+  
+}
