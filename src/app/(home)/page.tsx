3afd16e74fa36a426f6e1f29f0ead9b167fa3ab4
Wolace Luedke <wolacepin@hotmail.com>
2023-10-23 12:19:14 -0300

    feat: add footer components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -29,7 +29,7 @@ export default async function Home() {
             }
         }
     })
-return <div className='flex flex-col gap-8'>
+return <div className='flex flex-col gap-8 py-8'>
     <PromoBanner
     src="/banner-home-01.png"
     alt='até 55% de desconto em mouses!'
---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return ( 
+    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
+    © 2023 Copyright <span className="font-semibold"> Volac Store</span>
+    </div> );
+}
+ 
+export default Footer;
