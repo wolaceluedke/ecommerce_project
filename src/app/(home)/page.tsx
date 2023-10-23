b9f48bd33f4225f150ae473444c9de2aea248c11
Wolace Luedke <wolacepin@hotmail.com>
2023-10-22 22:24:23 -0300

    feat: add second to banner home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -24,8 +24,18 @@ return <div className=''>
     <Categories />
     </div>
     <div className='mt-8'>
+        <p className='font-bold uppercase pl-5 mb-3'>Ofertas</p>
         <ProductList products={deals} />
     </div>
+
+    <Image 
+    src="/banner-home-02.png"
+    height={0}
+    width={0}
+    className='h-auto w-full px-5'
+    sizes='100vw'
+    alt='até 55% desconto em mouses'
+    />
 </div>
   
 }
