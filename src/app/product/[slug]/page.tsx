14905e94f250fe85fa30d2edf63a082ffbe2f80a
Wolace Luedke <wolacepin@hotmail.com>
2023-10-24 20:47:11 -0300

    feat: ensure recomended products are shwon on product details page

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { prismaClient } from "@/lib/prisma";
 import ProductImages from "../components/procuct-images";
 import ProductInfo from "../components/product-info";
 import { computeProductTotalPrice } from "@/helpers/product";
+import ProductList from "@/components/ui/product-list";
 
 interface ProductDetailsPageProps {
     params: {
@@ -17,14 +18,30 @@ const ProductDetailsPage = async ({
         where: {
             slug: slug,
         },
+        include: {
+            category: {
+                include: {
+                    products: {
+                        where: {
+                            slug: {
+                                not: slug,
+                            }
+                        }
+                    }
+                }
+            }
+        }
     });
+
+    console.log(product)
     
     if(!product) return null;
 
     return <h1>
-        <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-8 pb-8">
             <ProductImages imageUrls={product.imageUrls} name={product.name} />
             <ProductInfo product={computeProductTotalPrice(product)} />
+            <ProductList products={product.category.products}/>
         </div>
     </h1>
 }
