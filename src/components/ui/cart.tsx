71178d648cdfbfafb25cd16d8372bdd7b37ba4da
Wolace Luedke <wolacepin@hotmail.com>
2023-10-30 18:08:50 -0300

    feat:  add CartItem component

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -2,12 +2,14 @@ import { ShoppingCartIcon } from "lucide-react";
 import { Badge } from "./badge";
 import { useContext } from "react";
 import { CartContext } from "@/providers/cart";
+import CartItem from "./cart-item";
+import { computeProductTotalPrice } from "@/helpers/product";
 
 const Cart = () => {
 const { products } = useContext(CartContext)
 
     return ( 
-    <div>
+    <div className="flex flex-col gap-5">
         <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
@@ -16,7 +18,14 @@ const { products } = useContext(CartContext)
         Catálogo
       </Badge>
 
-        {products.map(product => <h1>{product.name}</h1>)}
+        <div className="flex flex-col gap-5 ">
+        {products.map(product => (
+          <CartItem 
+          product={computeProductTotalPrice(product as any) as any} 
+          key={product.id}
+           />
+        ))}
+        </div>
     </div>
      );
 }
--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -1,14 +1,15 @@
 'use client'
 
+import { ProductWithTotalPrice } from "@/helpers/product";
 import { Product } from "@prisma/client";
 import { ReactNode, createContext, useState } from "react";
 
-interface CartProduct extends Product {
+export interface CartProduct extends ProductWithTotalPrice {
     quantity: number,
 }
 
 
-interface ICartContext {
+ interface ICartContext {
     products: CartProduct[];
     cartTotalPrice: number;
     cartBasePrice: number;
@@ -28,6 +29,23 @@ const CartProvider = ({children}: { children: ReactNode}) => {
     const [products, setProducts ] = useState<CartProduct[]>([])
 
     const addProductToCart = (product: CartProduct) => {
+
+        const productIsAlreadyOnCart = products.some(cartProduct => cartProduct.id == product.id) 
+
+        if(productIsAlreadyOnCart){
+            setProducts((prev) => 
+            prev.map((cartProduct) => {
+                if (cartProduct.id == product.id) {
+                    return {
+                        ...cartProduct, 
+                        quantity: cartProduct.quantity + product.quantity,
+                    }
+                }
+                return cartProduct
+            })) 
+            return
+        }
+
         setProducts((prev) => [...prev, product]);
     }
 
