2b0ac4e0817c6791eb6b48b8895169a6a97df7d1
Wolace Luedke <wolacepin@hotmail.com>
2023-12-12 23:22:41 -0300

    feat: add create order server action

--- src/actions/checkout.ts
+++ src/actions/checkout.ts
@@ -1,37 +1,40 @@
-"use server"
+"use server";
 
-import { CartProduct } from "@/providers/cart"
-import Stripe from "stripe"
+import { CartProduct } from "@/providers/cart";
+import Stripe from "stripe";
 
-export const createCheckout = async (products: CartProduct[]) => {
-    //CRIAR CHECKOUT
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-        apiVersion: "2023-10-16"
-    });
+export const createCheckout = async (
+  products: CartProduct[],
+  orderId: string,
+) => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2023-10-16",
+  });
 
+  const checkout = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    mode: "payment",
+    success_url: "http://localhost:3000",
+    cancel_url: "http://localhost:3000",
+    metadata: {
+      orderId,
+    },
+    line_items: products.map((product) => {
+      return {
+        price_data: {
+          currency: "brl",
+          product_data: {
+            name: product.name,
+            description: product.description,
+            images: product.imageUrls,
+          },
+          unit_amount: product.totalPrice * 100,
+        },
+        quantity: product.quantity,
+      };
+    }),
+  });
 
-    
-
-     const checkout = await stripe.checkout.sessions.create ({
-        payment_method_types: ["card"],
-        mode: 'payment',
-        success_url: process.env.HOST_URL,
-        cancel_url: process.env.HOST_URL,
-        line_items: products.map(product => {
-            return{
-                price_data: {
-                    currency: 'brl',
-                    product_data: {
-                        name: product.name,
-                        images: product.imageUrls,
-                        description: product.description
-                    },
-                    unit_amount: product.totalPrice * 100,
-                },
-                quantity: product.quantity,
-            }
-        }),
-    })
-    //RETORNAR CHECKOUT
-    return checkout
-}
+  // RETORNAR O CHECKOUT
+  return checkout;
+};
--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -1,95 +1,109 @@
-import { ShoppingCartIcon } from "lucide-react";
-import { Badge } from "./badge";
-import { useContext } from "react";
-import { CartContext } from "@/providers/cart";
-import CartItem from "./cart-item";
-import { computeProductTotalPrice } from "@/helpers/product";
-import { Separator } from "./separator";
-import { ScrollArea } from "./scroll-area";
-import { Button } from "./button";
-import { createCheckout } from "@/actions/checkout";
-import { loadStripe } from "@stripe/stripe-js";
+  import { ShoppingCartIcon } from "lucide-react";
+  import { Badge } from "./badge";
+  import { useContext } from "react";
+  import { CartContext } from "@/providers/cart";
+  import CartItem from "./cart-item";
+  import { computeProductTotalPrice } from "@/helpers/product";
+  import { Separator } from "./separator";
+  import { ScrollArea } from "./scroll-area";
+  import { Button } from "./button";
+  import { createCheckout } from "@/actions/checkout";
+  import { loadStripe } from "@stripe/stripe-js";
+  import { createOrder } from "@/actions/order";
+  import { useSession } from "next-auth/react";
 
-const Cart = () => {
-const { products, subtotal, total, totalDiscount } = useContext(CartContext)
+  const Cart = () => {
+    const { data } = useSession();
 
-const handleFinishPucharseClick = async () => {
- const checkout = await createCheckout(products)
+    const { products, subtotal, total, totalDiscount } = useContext(CartContext);
 
- const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
+    const handleFinishPurchaseClick = async () => {
+      if (!data?.user) {
+        // TODO: redirecionar para o login
+        return;
+      }
 
- stripe?.redirectToCheckout({
-  sessionId: checkout.id,
- })
-}
+      const order = await createOrder(products, (data?.user as any).id);
 
-    return ( 
-    <div className="flex flex-col gap-8 h-full">
-        <Badge
-        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
-        variant="outline"
-      >
-        <ShoppingCartIcon size={16} />
-        Catálogo
-      </Badge>
+      const checkout = await createCheckout(products, order.id);
 
-      <div className="flex h-full max-h-full flex-col gap-5 overflow-hidden">
-        <ScrollArea className="h-full">
-          <div className="flex h-full flex-col gap-8">
-            {products.length > 0 ? (
-              products.map((product) => (
-                <CartItem
-                  key={product.id}
-                  product={computeProductTotalPrice(product as any) as any}
-                />
-              ))
-            ) : (
-              <p className="text-center font-semibold">
-                Carrinho vazio. Vamos fazer compras?
-              </p>
-            )}
-          </div>
-        </ScrollArea>
-      </div>
-        {products.length > 0 && (
-          <div className="flex flex-col gap-3">
-          <Separator />
+      const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
-          <div className="flex items-center justify-between text-xs">
-              <p>Subtotal</p>
-              <p>R$ {subtotal.toFixed(2)}</p>
-          </div>
 
-          <Separator />
+      // Criar pedido no banco
 
-          <div className="flex items-center justify-between text-xs">
-              <p>Entrega</p>
-              <p>Grátis</p>
-          </div>
+  stripe?.redirectToCheckout({
+    sessionId: checkout.id,
+  })
+  }
 
-          <Separator />
+      return ( 
+      <div className="flex flex-col gap-8 h-full">
+          <Badge
+          className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+          variant="outline"
+        >
+          <ShoppingCartIcon size={16} />
+          Catálogo
+        </Badge>
 
-          <div className="flex items-center justify-between text-xs">
-              <p>Descontos</p>
-              <p>R$ {totalDiscount.toFixed(2)}</p>
-          </div>
-
-          <Separator />
-
-          <div className="flex items-center justify-between text-sm font-bold">
-              <p>Total</p>
-              <p>R$ {total.toFixed(2)}</p>
-          </div>
-
-          <Button 
-          className="uppercase font-bold mt-7" 
-          onClick={handleFinishPucharseClick}>
-            Finalizar Compra</Button>
-
+        <div className="flex h-full max-h-full flex-col gap-5 overflow-hidden">
+          <ScrollArea className="h-full">
+            <div className="flex h-full flex-col gap-8">
+              {products.length > 0 ? (
+                products.map((product) => (
+                  <CartItem
+                    key={product.id}
+                    product={computeProductTotalPrice(product as any) as any}
+                  />
+                ))
+              ) : (
+                <p className="text-center font-semibold">
+                  Carrinho vazio. Vamos fazer compras?
+                </p>
+              )}
+            </div>
+          </ScrollArea>
         </div>
-        )}
-    </div>
-     );
-}
- 
-export default Cart;
+          {products.length > 0 && (
+            <div className="flex flex-col gap-3">
+            <Separator />
+
+            <div className="flex items-center justify-between text-xs">
+                <p>Subtotal</p>
+                <p>R$ {subtotal.toFixed(2)}</p>
+            </div>
+
+            <Separator />
+
+            <div className="flex items-center justify-between text-xs">
+                <p>Entrega</p>
+                <p>Grátis</p>
+            </div>
+
+            <Separator />
+
+            <div className="flex items-center justify-between text-xs">
+                <p>Descontos</p>
+                <p>R$ {totalDiscount.toFixed(2)}</p>
+            </div>
+
+            <Separator />
+
+            <div className="flex items-center justify-between text-sm font-bold">
+                <p>Total</p>
+                <p>R$ {total.toFixed(2)}</p>
+            </div>
+
+            <Button 
+            className="uppercase font-bold mt-7" 
+            onClick={handleFinishPurchaseClick}>
+              Finalizar Compra</Button>
+
+          </div>
+          )}
+      </div>
+      );
+  }
+  
+  export default Cart;
